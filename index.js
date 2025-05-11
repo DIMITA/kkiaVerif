7650f3a58adc7b63a95141991cf5aceaa2579eb5
wilfried <dimitriomoralesr01@@gmail.com>
2025-05-11 05:52:30 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { Kkiapay } = require("@kkiapay/node-sdk");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Initialisation du SDK KKiaPay
+const kkiapay = kkiapay({
+    privatekey: process.env.privatekey,
+    publickey: process.env.publickey,
+    secretkey: process.env.secretkey
+  });
+
+app.get("/verifier-kkiapay", async (req, res) => {
+  const { transactionId } = req.query;
+
+  if (!transactionId) {
+    return res.status(400).json({ message: "transactionId manquant" });
+  }
+
+  try {
+    const transaction = await kkiapay.verifyTransaction(transactionId);
+    const redirect = "https://lunevelours.com/kreturn";
+
+    if (transaction.status === "SUCCESS") {
+      // ✅ Paiement validé : tu peux créer la commande Shopify ici
+      return res.redirect(redirect + "?status=success&tid=" + transactionId);
+    } else {
+        return res.redirect(redirect + "?status=failed");
+    }
+  } catch (err) {
+    console.error("Erreur KKiaPay :", err.message || err);
+    return res.status(500).send("❌ Erreur serveur.");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
