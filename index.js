2b3764192f4b339883db6a16a7625e20d68b36b5
wilfried <dimitriomoralesr01@@gmail.com>
2025-05-11 06:04:26 +0200

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import { kkiapay } from "@kkiapay/node-sdk";
+import { kkiapay } from "@kkiapay-org/nodejs-sdk";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Initialisation du SDK KKiaPay
-const kkiapay = kkiapay({
+const kkiapayLd = kkiapay({
     privatekey: process.env.privatekey,
     publickey: process.env.publickey,
     secretkey: process.env.secretkey
@@ -21,7 +21,7 @@ app.get("/verifier-kkiapay", async (req, res) => {
   }
 
   try {
-    const transaction = await kkiapay.verifyTransaction(transactionId);
+    const transaction = await kkiapayLd.verifyTransaction(transactionId);
     const redirect = "https://lunevelours.com/kreturn";
 
     if (transaction.status === "SUCCESS") {
